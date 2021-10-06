1a60e1fcafcb54404be67c35ef84b5745ffc10fa
YannisTzitzikas <tzitzik@ics.forth.gr>
2021-10-06 11:07:10 +0300

    test

---
+++ CS252playground/src/week1/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package week1;
+
+/**
+ * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

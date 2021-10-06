0ebd3d3c401c153d9a62f9c1d9ab990edde17f91
YannisTzitzikas <tzitzik@ics.forth.gr>
2021-10-06 12:47:58 +0300

    package for Strings

---
+++ CS252playground/src/frontistirioStrings/examples.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package frontistirioStrings;
+
+/**
+ * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
+ *
+ */
+public class examples {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

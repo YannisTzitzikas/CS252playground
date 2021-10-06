7f9a39aba010e4aaccff248877b1bc8eeb19515b
YannisTzitzikas <tzitzik@ics.forth.gr>
2021-10-06 11:17:25 +0300

    test

--- CS252playground/src/week1/HelloWorld.java
+++ CS252playground/src/week1/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("LALA");
+		System.out.println("Hello world");
 
 	}
 

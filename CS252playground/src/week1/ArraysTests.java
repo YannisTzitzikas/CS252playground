7f0f6198663846db457847448ff8f0c24f3d924e
YannisTzitzikas <tzitzik@ics.forth.gr>
2021-10-12 11:04:15 +0300

    More examples with arrays

--- CS252playground/src/week1/ArraysTests.java
+++ CS252playground/src/week1/ArraysTests.java
@@ -1,14 +1,94 @@
 package week1;
 
-
+class Foo {
+	
+}
 public class ArraysTests {
 
 	public static void main(String[] args ) {
+		System.out.println("Hello Arrays");
+		
+		// Pinakas AM foithton
+		// Dilwsh
+		
+		int K=10;
+		
+		int[]  students; // students---pointer--> antikeimeno typou pinakas akeraiwn
+		students = new int[K]; // δέσμευση χώρου
+		
+		System.out.println(students);
+		System.out.println(students[0]);  // arxikopoihsh timwn se 0
+		System.out.println(students[9]);  // out of bounds
+		//System.out.println(students[-1]);
+		
+		// Week days shortcuts
+		String[] days = new String[7];
+		days[0] = "Mon";
+		days[1] = "Tue";
+		days[2] = "Wed";
+		days[3] = "Thu";
+		days[4] = "Fri";
+		days[5] = "Sat";
+		days[6] = "Sun";
+		for (String d: days) {
+			System.out.print(d + " ");
+		}
+		for (int i=0; i<days.length; i++) {
+			System.out.print(days[i] + "_");
+		}
+		
+		// Declaration and initialization (automatic size calculation)
+		System.out.println();
+		String[] months = { "Jan", "Feb", "Aug1", "Aug2"  };
+		for (String m: months) {
+			System.out.println("Month:"+ m);
+			for (String d: days)
+				System.out.print(d + " ");
+			System.out.println();
+				
+		}
+		
+		// 2D, 3D, XD arrays
+		int[][] sodoku = new int[9][9];
+		//String[][] sodoku = new String[9][9];
+		//Foo[][] sodoku = new Foo[9][9];
+		
+		int[] oneSolution = {9,8,7,1,2,3,5,4,6,44444444};
+		int[] tooSmall = {1000};
+		
+		sodoku[6] = oneSolution;
+		sodoku[8] = tooSmall;
+		sodoku[8] = oneSolution;
+		sodoku[8][sodoku[8].length-1]=77;
+				
+		for (int i=0; i<sodoku.length; i++) {
+			for (int j=0; j < sodoku[i].length; j++) {
+			//for (int j=0; j < 9; j++) {	
+				System.out.print(sodoku[i][j] + " ");
+			}
+			System.out.println();
+		}
 		
 		
+		
+		System.out.println("\nBye bye Arrays");
+		
+		
+		
+		
+		
+		
+		
+		
+		System.exit(0);
+		
+		
+		// Αρχικοποίηση με τη δήλωση
 		int[] p = {22,0,0};
-		
 		//int[] p;		p = new int[3];		p[0]=22;
+		
+		
+		// Τρίλιζα
 		
 		//int[][] triliza = new int[3][3];
 		

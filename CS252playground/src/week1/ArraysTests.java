a4905dc34a82cfca2e53245b8395e63122ecd03d
YannisTzitzikas <tzitzik@ics.forth.gr>
2021-10-12 12:04:53 +0300

    Various examples with arrays

--- CS252playground/src/week1/ArraysTests.java
+++ CS252playground/src/week1/ArraysTests.java
@@ -1,5 +1,7 @@
 package week1;
 
+// Oct 12, 2021
+
 class Foo {
 	
 }
@@ -69,76 +71,52 @@ public class ArraysTests {
 			System.out.println();
 		}
 		
+		// 2D initialization
+		
+		String[][] triliza = {  
+				{"X","O","O"},
+				{"O","X","O","1","2","3"},
+				{"X","O","O"},
+	    };
+		
+		String[][] spy;
+		spy = triliza;
+		spy[0][0]="Bammm";
+		
+		for (String[] tr: triliza ) {
+			System.out.println("This line has size " +  tr.length);
+		}
 		
 		
-		System.out.println("\nBye bye Arrays");
-		
-		
-		
-		
-		
-		
-		
-		
-		System.exit(0);
-		
-		
-		// Αρχικοποίηση με τη δήλωση
-		int[] p = {22,0,0};
-		//int[] p;		p = new int[3];		p[0]=22;
-		
-		
-		// Τρίλιζα
-		
-		//int[][] triliza = new int[3][3];
-		
-		int[][] triliza = {  
-							{0,1,0},
-							{1,0,0,7,8,9},
-							{2,0,1},
-						  };
-		
-		
-		//int[] tmp  = {0,1,0};
-		//triliza[1] = tmp;
-		//triliza[1] = new int[1];
+		for (String[] tr: triliza) {
+			for (String tcell: tr) {
+				System.out.print(tcell);
+			}
+			System.out.print("\n");
+		}
+
 		
 		for (int i=0; i<triliza.length; i++) {
-			for (int j=0; j<triliza[i].length; j++) 
-				System.out.print(triliza[i][j]+ " ");
-			System.out.println();
+			for (int j=0; j< triliza[i].length; j++) {
+				System.out.print(triliza[i][j]);
+			}
+			System.out.print("\n");
 		}
-		//System.out.print(triliza[0][4]);
+			
 		
-		//main(null);
+		for (int curVal :  new int[]  {4,3,2,1} ) {
+			System.out.println(curVal);
+		}
+
+		Object tapantaola;
+		tapantaola = triliza;
+		tapantaola = "lala";
 		
-		/*
-		
-		// ΔΗΛΩΣΕΙΣ
-		int[] p1 = {2,1,4,5,6};
-		
-		int[][] p2 =  {    
-				        {2,1,7},
-				        {4,5}
-				      } ;
-		
-		int[][] p3 = new int[2][3];
-		
-		
-		int[][] p4 = new int[2][];
-		
-		p4[0] = new int[30];
-		
-		
-		System.out.println(p2[0].length);
-		System.out.println(p2[1].length);
-		
-		
-		System.out.println(p3[0].length);
-		System.out.println(p3[1].length);
-		
-		System.out.println("Ola kala!");
-		*/
-	}
+			
+		System.out.println("\nBye bye Arrays");
+		//	System.exit(0);
+			
+	
+	} // main
 	
 }

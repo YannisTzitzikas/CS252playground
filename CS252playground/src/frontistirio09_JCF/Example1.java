50df5370ca3177dd9606d76fbbda15a650176ffd
YannisTzitzikas <tzitzik@ics.forth.gr>
2021-12-09 12:34:56 +0200

    package for JCF (Java Collection Framework)

---
+++ CS252playground/src/frontistirio09_JCF/Example1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package frontistirio09_JCF;
+
+/**
+ * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
+ *
+ */
+public class Example1 {
+
+	/**
+	 * 
+	 */
+	public Example1() {
+		// TODO Auto-generated constructor stub
+	}
+
+}

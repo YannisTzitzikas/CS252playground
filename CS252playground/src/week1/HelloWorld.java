448e34492a26e9ffeb9093fb259d7f90a8bd9495
YannisTzitzikas <tzitzik@ics.forth.gr>
2022-09-26 11:43:00 +0300

    Update of message

--- CS252playground/src/week1/HelloWorld.java
+++ CS252playground/src/week1/HelloWorld.java
@@ -9,6 +9,6 @@ package week1;
  */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world Sept 2022!");
 	}
 }

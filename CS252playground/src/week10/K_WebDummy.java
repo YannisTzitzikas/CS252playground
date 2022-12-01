755557dc3cea675740c0767d62e97302ee89cd30
YannisTzitzikas <tzitzik@ics.forth.gr>
2022-12-01 11:22:27 +0200

    Example with a simple web server

---
+++ CS252playground/src/week10/K_WebDummy.java
@@ -0,0 +1,78 @@
+package week10;
+
+/**
+ * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
+ * A simple client-server example that uses threads
+ */
+
+// simple computational task
+class ComputationalTask {
+	static void work(String request) {	
+		System.out.println("Server: Starting working on request " + request);
+		try {
+			Thread.sleep(550); //50 ok
+			System.out.println(request);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Server: Comletion of request " + request);			
+	}
+}
+
+// computational task wrapped to run in a new thread
+class TheadedComputationTask extends Thread {
+	String request;
+	TheadedComputationTask(String request) {this.request=request;}
+	public void run() {
+		ComputationalTask.work(request);
+	}
+}
+
+class WebServer {
+	static String service(String request) {
+		//A: 
+		//ComputationalTask.work(request);  // no thread (single instruction execution)
+		
+		//B:  by defining a new class that extends Thread
+		//	TheadedComputationTask tct = new TheadedComputationTask(request);
+		//tct.start();
+		
+		//C: Through inner classes
+		
+		new Thread( 
+				new Runnable() {
+					public void run() {
+						ComputationalTask.work(request);
+					}
+				}
+		).start();
+		
+		/*
+		new Thread( () ->  {ComputationalTask.work(request);}).start();  // lamba expression of Java >=8
+		*/
+		return "202";
+	}
+	
+}
+
+class ClientBrowser {
+	static int id=0;
+	ClientBrowser() {
+		id++;
+		System.out.println("Client " + id + " started");
+		String response = WebServer.service("Request from client " + id);
+		//System.out.println(response);
+	}
+}
+
+
+
+class WebTester {
+	public static void main(String[] lala) {
+		int K=100000;
+		for (int i=0;i<K;i++) {
+			ClientBrowser cb = new ClientBrowser();
+		}
+	}
+}

921834c4d0fbf37c9639c9086e5a005fcb96d82a
YannisTzitzikas <tzitzik@ics.forth.gr>
2024-09-21 22:11:17 +0300

    test

--- CS252playground/src/week1/HelloWorld.java
+++ CS252playground/src/week1/HelloWorld.java
@@ -3,13 +3,9 @@
  */
 package week1;
 
-/**
- * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
- *
- */
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world Sept 2022!");
+		System.out.println("Hello world Sept 2023!");
 		
 	}
 }

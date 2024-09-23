20b4af04dde960329c20c692cc0d08d476d1d009
YannisTzitzikas <tzitzik@ics.forth.gr>
2024-09-23 23:04:42 +0300

    test

---
+++ CS252playground/src/week1/Class2023.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package week1;
+
+
+public class Class2023 {
+	 public static void main(String[] lala) {
+		 for (int i=1;i<101;i++) {
+			 if (i%7==0) {
+				 System.out.println();
+			 }
+			 System.out.print(i + " ");
+		 }
+		 
+	 }
+
+}

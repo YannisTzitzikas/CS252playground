fbeb9b51c7ab0695371a505f56c7677d0659e605
YannisTzitzikas <tzitzik@ics.forth.gr>
2024-09-23 22:48:51 +0300

    Example of reading from console

---
+++ CS252playground/src/week1/ReadFromConsole.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package week1;
+
+import java.util.Scanner;
+
+/**
+ * @author Yannis Tzitzikas (yannistzitzik@gmail.com)
+ *
+ */
+public class ReadFromConsole {
+
+	 public static void main(String[] lala) {
+		 System.out.println("READING FROM CONSOLE");
+		 
+		 
+		
+		 System.out.print("Give me a string: ");
+		 Scanner in = new Scanner(System.in);
+		 String s = in.next(); 
+		 System.out.println("You typed " + s);
+		 
+		 
+		 System.out.print("Give me an integer: ");
+		 int a = in.nextInt();
+		 System.out.println("You typed " + a);
+
+		 // input checking method
+		 
+		 a=-1;
+		 do {
+		  System.out.print("Give me a positive integer integer: ");
+		  s = in.next();
+		  try {
+	            a = Integer.parseInt(s);
+	            System.out.println(s + " is a valid integer");
+	        } catch (NumberFormatException e) {
+	            System.out.println(s + " is not a valid integer");
+	        }
+		 } while (a<0); 
+		 System.out.println("Bye bye");
+	 }
+
+}

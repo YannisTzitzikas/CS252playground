dff577cbae5994c05ff5ce3df02bc226a6d3fa95
YannisTzitzikas <tzitzik@ics.forth.gr>
2024-12-24 23:15:36 +0200

    Christmas tree improvements

--- CS252playground/src/appChristmasTree/RecursiveTriangleExt.java
+++ CS252playground/src/appChristmasTree/RecursiveTriangleExt.java
@@ -13,11 +13,11 @@ import javax.swing.JOptionPane;
 
 
  class ChristmasTree {
-	 static int N = 800; // canvas size
+	 static int N = 900; // canvas size
 	 static Picture pic = new Picture(N, N);  // new picture object
 	 static Pixel[][] pixels; // array for holding the pixels
-	 static int MIN_RADIUS = 10 ; // gia tis xristougenniatikes mpales
-	 static int MAX_RADIUS = 20; // gia tis xristougenniatikes mpales
+	 static int MIN_RADIUS = 2 ; // gia tis xristougenniatikes mpales
+	 static int MAX_RADIUS = 30; // gia tis xristougenniatikes mpales
 	 
 	 public static void StartRecursion(int N, Color col){
          recursiveTriangle(pixels[(N/2)-1][N-1],pixels[0][0],pixels[N-1][0], col);
@@ -45,20 +45,40 @@ import javax.swing.JOptionPane;
         //(p3+p1)/2
         pixels[(p3.getX()+p1.getX())/2][(p3.getY()+p1.getY())/2].setColor(col);
         
+        /*
+        // snow
+        int Xsnow = (int) ((p1.getX()+p2.getX()+p3.getX())/3.2);
+        int Ysnow = (int) ((p1.getY()+p3.getY()+p2.getY())/3.4);
+        for (int k=0;k<12;k++)
+        	  pixels[(Xsnow-k>0)?Xsnow-k:0][(Ysnow-k>0)?Ysnow-k:0].setColor(Color.yellow);
+        */
         
         
-        // drawing a circle
+        // drawing a circle (Christmas ball)
         if (Math.random() < 0.2) { // for not placing balls everywhere
 	        int tmpRadius = p3.getX()    - p2.getX();
 	        int centerX =  (p2.getX()+p3.getX())/2;
 	        int centerY = (p1.getY()+p2.getY())/2;
-	        Color c = Color.red;
-	        if (Math.random() < 0.3) {  // for getting red and blue balls randomly
+	        Color c;
+	        double randVal = Math.random();
+	        if (randVal<0.4) 
+	        	c = Color.red;
+	        else if ((randVal>= 0.4) &&  (randVal<0.6))
+	        	c = Color.GREEN;
+	        else
 	        	c = Color.blue;
-	        }
 	       	setPixelsCircle(tmpRadius,  centerX, centerY, c);
         } 
         
+        // snowflakes
+        // drawing a circle (Christmas ball)
+        if (Math.random() < 0.2) { // for not placing snow everywhere
+	        int tmpRadius = 6; // p3.getX()    - p2.getX();
+	        int centerX =  (p2.getX()+p3.getX())/2;
+	        int centerY = (p1.getY()+p2.getY())/2;
+	        Color c=Color.white;
+	        setPixelsCircle(tmpRadius,  centerX, centerY, c);
+        }
         
         // recursive calls
         recursiveTriangle(  pixels[(p1.getX()+p2.getX())/2][(p1.getY()+p2.getY())/2],
@@ -97,11 +117,17 @@ import javax.swing.JOptionPane;
      double x;
      double y; 
      double numOfRings=3; // how many rings to use for drawing a ball
+     int numOfInternalCircles=10;
+     
+     for (int h = 0; h<numOfInternalCircles; h++) {
+    	 
+      radius = radius - 4;
+    	 
       if ((radius >= MIN_RADIUS) && (radius < MAX_RADIUS))  {
-
+	  
     	for (int j=0;j<numOfRings;j++)   {
 	    	for (int i=0; i<=360; i++){	
-	    		x =  centerX + ((radius-j)* Math.cos(Math.toRadians((int)i)));
+	    		x =  (centerX + ((radius-j)* Math.cos(Math.toRadians((int)i))));
 	    		y =  centerY + ((radius-j)* Math.sin(Math.toRadians((int)i)));
 	    		if (x>0 && y>0 && x<N && y<N) { // fits in canvas		    		
 	    			pixels[Math.abs((int)x)][Math.abs((int)y)].setColor(col);	
@@ -110,6 +136,8 @@ import javax.swing.JOptionPane;
     	}
     	
      }
+     } // internal 
+
    }
     
     
@@ -133,10 +161,10 @@ import javax.swing.JOptionPane;
     	pic.setOriginLowerLeft();   // setting the space of coordinates
 	    pixels=new Pixel[N][N]; // array for holding the pixels
 	    for (int i=0; i<2000;i++) {
-		    setBackgroundPixels(N, Color.white); // background color
-		    StartRecursion(N, Color.green); // drawing the Christmas tree
+		    setBackgroundPixels(N, Color.black); // background color
+		    StartRecursion(N, Color.YELLOW); // drawing the Christmas tree
 		    displayPicture() ;
-		    try {   Thread.sleep(100); } catch (Exception e ) { }
+		    try {   Thread.sleep(150); } catch (Exception e ) { }
 		    
 	    }
     }  

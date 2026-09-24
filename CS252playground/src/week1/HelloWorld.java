8b07e98fa578717edb9249687ee4192f60f91257
YannisTzitzikas <tzitzik@ics.forth.gr>
2026-09-24 10:30:55 +0300

    update

--- CS252playground/src/week1/HelloWorld.java
+++ CS252playground/src/week1/HelloWorld.java
@@ -5,11 +5,17 @@ package week1;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world Sept 2024!");
+		System.out.println("Hello world Sept 2026!");
 		
-		boolean b = (0.2 +0.1) == 0.3;
-		//boolean b = ((0.2 +0.1) -0.3 )<0.000001;
+		double a = 3.14;
 		
+		System.out.println(a);
+		System.out.println(0.2 + 0.1);
+		System.out.println((0.2 + 0.1)==0.3);
+		
+		
+		//boolean b = (0.2 +0.1) == 0.3;
+		boolean b = ((0.2 +0.1) -0.3 )<0.000001;
 		System.out.println(b);
 		
 	}
